4d2f7c8657b35d614caa51f8327485ca83feb8cb
Danishhussain6 <dh858168@gmail.com>
2026-01-28 21:06:59 +0530

    updated

---
+++ pattern printing/arrays/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a[10][10], transpose[10][10], row, column, i, j;
+
+   cout << "Enter rows and columns of matrix: ";
+   cin >> row >> column;
