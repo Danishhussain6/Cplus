0ff12d038efe5db72d47da9db0e590fb725dae11
Danishhussain6 <dh858168@gmail.com>
2026-01-28 21:09:07 +0530

    Delete pattern printing/arrays directory

--- pattern printing/arrays/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-   int a[10][10], transpose[10][10], row, column, i, j;
-
-   cout << "Enter rows and columns of matrix: ";
-   cin >> row >> column;
